87d79fc5df8c4545cab14a1e6dc65842fbe1a011
salvatore <salvatore.cicciu@gmail.com>
2017-09-28 14:44:55 +0200

    Add feature to allow to run with python2 and python 3 and update string of warning

--- parser.py
+++ parser.py
@@ -1,8 +1,9 @@
+from __future__ import print_function #allow to run with python 2.7 and python 3
 import json
 from pprint import pprint
 
+
 class Color:
-   PURPLE = '\033[95m'
    BLUE = '\033[94m'
    GREEN = '\033[92m'
    YELLOW = '\033[93m'
@@ -10,7 +11,7 @@ class Color:
 
 
 #Open JSON File
-with open('src/assets/data/libellID.json') as data_file:    
+with open('src/assets/data/libellID.json') as data_file:
     data = json.load(data_file)
 
 #Length of criterias
@@ -20,7 +21,7 @@ dragonflieName=""
 dragonflies = data['dragonflies']
 nbError=0
 
-#Verif all number of criterias each dragonflies 
+#Verif all number of criterias each dragonflies
 for dragonflie in dragonflies:
     #get number of criterias in dragonflie and get name of dragonflies
     nbCriteriasDragonflie = len(dragonflie['criteria'])
@@ -28,19 +29,19 @@ for dragonflie in dragonflies:
 
     #verif all number of crietrias each dragonflies as the same of number of total criterias
     if nbCriterias != nbCriteriasDragonflie:
-        print Color.BLUE+"Dragonflies "+dragonflieName+":"+Color.RED+"\t Warning, number of criterias as not correct\r\n"
+        print(Color.BLUE+"Dragonflies "+dragonflieName+":"+Color.RED+"Warning, number of criterias as not correct\r\n")
         nbError=nbError+1
-    
+
     #verif all number of value each criterias of dragonflies
     criteriasDragonflie = dragonflie['criteria']
     for idx,tupleCriteria in enumerate(criteriasDragonflie):
 
         #read tuple and check if the value are correct
         for valCriteria in tupleCriteria:
-            if len(criterias[idx]['values'])<= valCriteria:
-                print Color.BLUE+"Dragonflies "+dragonflieName+":"+Color.RED+"\t Warning, value of criteria number "+ Color.YELLOW + str(idx)+ Color.RED +" is too high\r\n"
+            if len(criterias[idx]['values'])<= valCriteria:#Warning, criteria xx is to high
+                print(Color.BLUE+"Dragonflies "+dragonflieName+":"+Color.RED+" Warning, criteria "+ Color.YELLOW + str(idx)+'.'+criterias[idx]['name'] + Color.RED +" is too high\r\n")
                 nbError=nbError+1
 if nbError>0:
-    print Color.RED+"They are "+str(nbError)+" warning"
+    print(Color.RED+"They are "+str(nbError)+" warning")
 else:
-    print Color.GREEN+"No warning! :)"
+    print(Color.GREEN+"No warning! :)")
