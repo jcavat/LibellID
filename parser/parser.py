c2494e52e73ff4394a7a339f7ea0adc3162aa3e4
ciccius <salvatore.cicciu@gmail.com>
2017-11-03 15:51:12 +0100

    Update parser.py with FlyPeriod control #17

--- parser/parser.py
+++ parser/parser.py
@@ -21,15 +21,19 @@ dragonflieName=""
 dragonflies = data['dragonflies']
 nbError=0
 
+nbWeekOfFlyPeriod = 4
+nbMounthOfFlyPeriod = 8
+
 #Verif all number of criterias each dragonflies
 for dragonflie in dragonflies:
+    ############################CRITERIA########################################
     #get number of criterias in dragonflie and get name of dragonflies
     nbCriteriasDragonflie = len(dragonflie['criteria'])
     dragonflieName = str(dragonflie['commonName'].encode('utf-8'))
 
     #verif all number of crietrias each dragonflies as the same of number of total criterias
     if nbCriterias != nbCriteriasDragonflie:
-        print(Color.BLUE+"Dragonflies "+dragonflieName+":"+Color.RED+"Warning, number of criterias as not correct\r\n")
+        print(Color.BLUE+"Dragonflies "+dragonflieName+": "+Color.RED+"Warning, number of criterias as not correct\n\r")
         nbError=nbError+1
 
     #verif all number of value each criterias of dragonflies
@@ -39,11 +43,27 @@ for dragonflie in dragonflies:
         #read tuple and check if the value are correct
         for valCriteria in tupleCriteria:
             if len(criterias[idx]['values'])<= valCriteria:#Warning, criteria xx is to high
-                print(Color.BLUE+"Dragonflies "+dragonflieName+":"+Color.RED+" Warning, criteria "+ Color.YELLOW + str(idx)+'.'+criterias[idx]['name'] + Color.RED +" is too high\r\n")
+                print(Color.BLUE+"Dragonflies "+dragonflieName+": "+Color.RED+"Warning, criteria "+ Color.YELLOW + str(idx)+'.'+criterias[idx]['name'] + Color.RED +" is too high \n\r")
                 nbError=nbError+1
+
+
+    ############################FLYPERIOD######################################
+    #verif all number of value each flyperiod
+    flyPeriodDragonflie = dragonflie['flyPeriod']
+    if(len(flyPeriodDragonflie)!=8):
+        print(Color.BLUE+"Dragonflies "+dragonflieName+": "+Color.RED+"Warning, number of month in flyPeriod is not correct \n\r")
+        nbError=nbError+1
+
+    for tupleFlyPeriod in enumerate(flyPeriodDragonflie):
+        if(len(tupleFlyPeriod[1]) != nbWeekOfFlyPeriod):
+            print(Color.BLUE+"Dragonflies "+dragonflieName+": "+Color.RED+"Warning, number of week in flyPeriod is not correct \n\r")
+            nbError=nbError+1
+
 if nbError>0:
-    print(Color.RED+"They are "+str(nbError)+" warning in libellID.json")
-    exit(1) #exit with error
+    print(Color.RED+"They are "+str(nbError)+" warning in libellID.json \n\r")
+    #exit(1) #exit with error
+    print("ERROR")
 else:
-    print(Color.GREEN+"No warning in libellID.json! :)")
-    exit(0) #exit without any error
+    print(Color.GREEN+"No warning in libellID.json! : \n\r")
+    print("NO_ERROR")
+    #exit(0) #exit without any error
