ee0f3939b1387d650fa982fe7cc8753e05c07534
ciccius <salvatore.cicciu@gmail.com>
2017-11-03 15:53:20 +0100

    Add npm execution for parser #17

--- parser/parser.js
+++ parser/parser.js
@@ -1,6 +1,18 @@
+var path="parser/parser.py"
 var PythonShell = require('python-shell');
+
+var pyshell = new PythonShell(path);
  
-PythonShell.run('parser/parser.py', function (err, results) {
-  if (err) throw err;
-  console.log('Results of parser: \n\r%s', results);
+pyshell.on('message', function (message) {
+  // received a message sent from the Python script (a simple "print" statement)
+  if(message=="ERROR"){
+    console.log("ERROR")
+    process.exit(1);
+  }else if(message =="NO_ERROR"){
+    console.log("NO_ERROR");
+    process.exit(0);
+  }else{
+    console.log(message);
+  }
 });
+
